cd7b45174a557eca52e1aaff818c09309560dfeb
Zibeline <zibeline.dev@gmail.com>
2025-03-10 18:16:16 +0100

    🏗️🪡 Add a hidden cheat option

--- autoloads/currency/currency.gd
+++ autoloads/currency/currency.gd
@@ -8,12 +8,37 @@ signal currency_updated(new_value : int)
 
 
 var currency : int
+var cheat_progress : int = 0
+var cheat_interval : float = 0.0
+var cheated : bool = false
 
 
 func initialise() -> void :
-	currency = 15000
+	currency = 650
 	
 	currency_updated.emit(currency)
+
+
+func _process(delta : float) -> void : 
+	if cheat_progress >= 1 :
+		cheat_interval -= delta
+		if cheat_interval <= 0.0 : 
+			cheat_progress = 0
+		if cheat_progress >= 3 :
+			cheat_progress = 0
+			cheat()
+
+
+func _input(event : InputEvent) -> void :
+	if event.is_action_pressed("stats") : 
+		cheat_progress += 1 
+		cheat_interval = 0.75
+
+
+func cheat() -> void : 
+	if not cheated : 
+		create_currency(350 + 4000)
+		cheated = true
 
 
 func create_currency(quantity : int) -> void : 
